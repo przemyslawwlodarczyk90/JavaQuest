c33caaf412e157eacf8cf739aaf2e40b5fb11e19
przemyslawwlodarczyk90 <przemyslawwlodarczyk90@wp.pl>
2025-04-02 21:35:18 +0200

    Initial commit

---
+++ src/main/java/com/example/javaquest/JavaQuestApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaquest/JavaQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
